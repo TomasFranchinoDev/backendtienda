44aa389ad2c17d23f69c23ba944691e12b3dcd62
TomasFranchino <tomi.franc2@gmail.com>
2026-02-02 16:05:47 -0300

    Backend settings v3 + runtime listo deploy

--- config/settings.py
+++ config/settings.py
@@ -76,6 +76,7 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware', # <--- Ponlo lo más arriba posible
 
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -156,7 +157,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # ============================================================================
 # MEDIA FILES & CLOUDINARY CONFIGURATION
 # ============================================================================
